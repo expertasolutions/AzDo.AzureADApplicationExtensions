2a1d5c81dda818a1a86e740cb05972fa3d096858
lturmel <lturmel@expertasolutions.com>
2019-02-02 15:34:22 -0500

    fix try
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- GetAzureAdApplicationId/src/createAzureApp.ps1
+++ GetAzureAdApplicationId/src/createAzureApp.ps1
@@ -19,40 +19,40 @@ write-host $tenantId
 write-host $applicationName
 
 $loginResult = az login --service-principal -u $servicePrincipalId -p $servicePrincipalKey --tenant $tenantId
-#az account set --subscription $subscriptionId
+$setAccountSubResult = az account set --subscription $subscriptionId
 
-#try {
-#    $test = az --version
-#} catch {
-#    write-host "Azure Cli not installed"
-#    throw;
-#}
+try {
+    $test = az --version
+} catch {
+    write-host "Azure Cli not installed"
+    throw;
+}
 
-#$versionResult = az --version
-#$result = [regex]::Match($versionResult, "azure-cli \((([0-9]*).([0-9]*).([0-9]*))\)").captures.groups
+$versionResult = az --version
+$result = [regex]::Match($versionResult, "azure-cli \((([0-9]*).([0-9]*).([0-9]*))\)").captures.groups
 
-#if($result.length -eq 5)
-#{
-#    $major = $result[2].value
-#    $minor = $result[3].Value
-#    $build = $result[4].value
-#}
-#$goodVersion = $false
+if($result.length -eq 5)
+{
+    $major = $result[2].value
+    $minor = $result[3].Value
+    $build = $result[4].value
+}
+$goodVersion = $false
 
-#write-host "Azure Cli Version '$major.$minor.$build' installed on build agent"
+write-host "Azure Cli Version '$major.$minor.$build' installed on build agent"
 
-#$applicationInfo = (az ad app list --filter "displayName eq '$applicationName'") | ConvertFrom-Json
-#$permissionAccessJson = $applicationInfo.oauth2Permissions | ConvertTo-Json -Compress
-#if($applicationInfo.oauth2Permissions.count -eq 1){
-#    $permissionAccessJson = "[" + $permissionAccessJson + "]"
-#}
+$applicationInfo = (az ad app list --filter "displayName eq '$applicationName'") | ConvertFrom-Json
+$permissionAccessJson = $applicationInfo.oauth2Permissions | ConvertTo-Json -Compress
+if($applicationInfo.oauth2Permissions.count -eq 1){
+    $permissionAccessJson = "[" + $permissionAccessJson + "]"
+}
 
-#if($applicationInfo.Length -eq 0) {
-#  write-host "Azure Ad Application named '$applicationName' doesn't exists"
-#  exit 1
-#}
+if($applicationInfo.Length -eq 0) {
+  write-host "Azure Ad Application named '$applicationName' doesn't exists"
+  exit 1
+}
 
-#write-host "Azure ApplicationID: $($applicationInfo.appId)"
-#write-host "Azure Permission Access Info-json: $($permissionAccessJson)"
+write-host "Azure ApplicationID: $($applicationInfo.appId)"
+write-host "Azure Permission Access Info-json: $($permissionAccessJson)"
 
 az account clear
