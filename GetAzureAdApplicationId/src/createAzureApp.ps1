b93c4d623fd8af4081a2bcb4b50af4ad940966a5
lturmel <lturmel@expertasolutions.com>
2019-02-02 15:37:22 -0500

    fix try
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- GetAzureAdApplicationId/src/createAzureApp.ps1
+++ GetAzureAdApplicationId/src/createAzureApp.ps1
@@ -11,7 +11,6 @@ param(
     [string]$applicationName
 )
 
-write-host "inside the task"
 write-host $subscriptionId
 write-host $servicePrincipalId
 write-host $servicePrincipalKey
@@ -19,33 +18,35 @@ write-host $tenantId
 write-host $applicationName
 
 $loginResult = az login --service-principal -u $servicePrincipalId -p $servicePrincipalKey --tenant $tenantId
-$setAccountSubResult = az account set --subscription $subscriptionId
+az account set --subscription $subscriptionId
 
 try {
     $test = az --version
+    write-host $test
 } catch {
     write-host "Azure Cli not installed"
     throw;
 }
 
 $versionResult = az --version
-$result = [regex]::Match($versionResult, "azure-cli \((([0-9]*).([0-9]*).([0-9]*))\)").captures.groups
+write-host $versionResult
+#$result = [regex]::Match($versionResult, "azure-cli \((([0-9]*).([0-9]*).([0-9]*))\)").captures.groups
 
-if($result.length -eq 5)
-{
-    $major = $result[2].value
-    $minor = $result[3].Value
-    $build = $result[4].value
-}
-$goodVersion = $false
+#if($result.length -eq 5)
+#{
+#    $major = $result[2].value
+#    $minor = $result[3].Value
+#    $build = $result[4].value
+#}
+#$goodVersion = $false
 
-write-host "Azure Cli Version '$major.$minor.$build' installed on build agent"
+#write-host "Azure Cli Version '$major.$minor.$build' installed on build agent"
 
 $applicationInfo = (az ad app list --filter "displayName eq '$applicationName'") | ConvertFrom-Json
-$permissionAccessJson = $applicationInfo.oauth2Permissions | ConvertTo-Json -Compress
-if($applicationInfo.oauth2Permissions.count -eq 1){
-    $permissionAccessJson = "[" + $permissionAccessJson + "]"
-}
+#$permissionAccessJson = $applicationInfo.oauth2Permissions | ConvertTo-Json -Compress
+#if($applicationInfo.oauth2Permissions.count -eq 1){
+#    $permissionAccessJson = "[" + $permissionAccessJson + "]"
+#}
 
 if($applicationInfo.Length -eq 0) {
   write-host "Azure Ad Application named '$applicationName' doesn't exists"
