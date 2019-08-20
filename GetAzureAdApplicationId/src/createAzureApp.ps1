8e6d82d1c3e9dac912a2b55fefe2b92399f11e71
lturmel <lturmel@expertasolutions.com>
2019-08-19 22:27:19 -0400

    remove everythings
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- GetAzureAdApplicationId/src/createAzureApp.ps1
+++ GetAzureAdApplicationId/src/createAzureApp.ps1
@@ -11,8 +11,8 @@ param(
     [string]$applicationName
 )
 
-az login --service-principal -u $servicePrincipalId -p $servicePrincipalKey --tenant $tenantId | Out-Null
-az account set --subscription $subscriptionId | Out-Null
+#az login --service-principal -u $servicePrincipalId -p $servicePrincipalKey --tenant $tenantId | Out-Null
+#az account set --subscription $subscriptionId | Out-Null
 
 try {
     az --version | Out-Null
@@ -20,22 +20,24 @@ try {
     write-host "Azure Cli not installed"
     throw;
 }
+
 try {
-  $applicationInfo = (az ad app list --filter "displayName eq '$applicationName'") | ConvertFrom-Json
-  $permissionAccessJson = $applicationInfo.oauth2Permissions | ConvertTo-Json -Compress
-  if($applicationInfo.oauth2Permissions.count -eq 1){
-      $permissionAccessJson = "[" + $permissionAccessJson + "]"
-  }
+  #$applicationInfo = (az ad app list --filter "displayName eq '$applicationName'") | ConvertFrom-Json
+  #$permissionAccessJson = $applicationInfo.oauth2Permissions | ConvertTo-Json -Compress
+  #if($applicationInfo.oauth2Permissions.count -eq 1){
+  #    $permissionAccessJson = "[" + $permissionAccessJson + "]"
+  #}
 
-  if($applicationInfo.Length -eq 0) {
-    write-host "Azure Ad Application named '$applicationName' doesn't exists"
-    exit 1
-  }
+  #if($applicationInfo.Length -eq 0) {
+  #  write-host "Azure Ad Application named '$applicationName' doesn't exists"
+  #  exit 1
+  #}
 
-  write-host "Azure ApplicationID: $($applicationInfo.appId)"
-  write-host "Azure Permission Access Info-json: $($permissionAccessJson)"
+  #write-host "Azure ApplicationID: $($applicationInfo.appId)"
+  #write-host "Azure Permission Access Info-json: $($permissionAccessJson)"
+
+  #az account clear | Out-Null
 }
 catch {
   write-host "error in ps"
 }
-$logoutResult = az account clear
